157221a626185eacf8bc147f5759024c27b61c54
ＭichaelMac <michaeltainan@gmail.com>
2018-06-28 18:56:23 +0800

    Tutorial 4 Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',  //The CSS element selector, 'app-heroes', matches the name of the HTML element that identifies this component within a parent component's template.
@@ -7,13 +8,19 @@ import { Hero } from 'src/app/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  // hero:Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
 
   // The ngOnInit is a lifecycle hook Angular calls ngOnInit shortly after creating a component. It's a good place to put initialization logic.
   ngOnInit() {
